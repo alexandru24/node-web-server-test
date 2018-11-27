1ba3d15aa5184afb0ca65fef2f20068984d4afdf
duduialaAlexandru <alexandru24@live.com>
2018-11-27 20:29:21 +0200

    Slight modification to server.js

--- server.js
+++ server.js
@@ -57,7 +57,9 @@ app.get('/info', (req, res)=>{
    })
 })
 app.get('/projects', (req, res)=>{
-    res.render('projects.hbs')
+    res.render('projects.hbs', {
+        titlePage: 'Welcome to the Projects page'
+    })
 })
 
 app.get('/error', (req, res)=>{
